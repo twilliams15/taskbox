a3a0009c429df557b2c71890f34522e97dc0aa75
Todd Williams <twilliams@gofundme.com>
2020-09-23 20:01:28 -0700

    adds storybook static assets

---
+++ storybook-static/precache-manifest.7720a89ce61085fdd4641ac4e709bd50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472c5e84fff099fa4fdb347a53c7ddf4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.dc8cb44086ebb24c13bd.bundle.js"
+  },
+  {
+    "url": "runtime~main.dc8cb44086ebb24c13bd.bundle.js"
+  },
+  {
+    "revision": "a1cae7ea234d8fb9d28d",
+    "url": "static/css/main.281f400c.chunk.css"
+  },
+  {
+    "url": "vendors~main.dc8cb44086ebb24c13bd.bundle.js"
+  },
+  {
+    "url": "vendors~main.dc8cb44086ebb24c13bd.bundle.js.LICENSE.txt"
+  }
+]);
