2377b90c6547b4a1cc32f56c1d518a781d4ad6be
Todd Williams <twilliams@gofundme.com>
2020-09-23 19:50:16 -0700

    another chapter done

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import TaskList from './TaskList';
-import * as TaskStories from './Task.stories';
+import {PureTaskList} from "./TaskList";
+import * as TaskStories from "./Task.stories";
 
 export default {
-    component: TaskList,
+    component: PureTaskList,
     title: "TaskList",
-    decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
+    decorators: [story => <div style={{padding: "3rem"}}>{story()}</div>],
 };
 
-const Template = args => <TaskList {...args} />;
+const Template = args => <PureTaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
     tasks: [
-        { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
-        { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
-        { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
-        { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
-        { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
-        { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+        {...TaskStories.Default.args.task, id: "1", title: "Task 1"},
+        {...TaskStories.Default.args.task, id: "2", title: "Task 2"},
+        {...TaskStories.Default.args.task, id: "3", title: "Task 3"},
+        {...TaskStories.Default.args.task, id: "4", title: "Task 4"},
+        {...TaskStories.Default.args.task, id: "5", title: "Task 5"},
+        {...TaskStories.Default.args.task, id: "6", title: "Task 6"},
     ],
 };
 
@@ -27,7 +27,7 @@ export const WithPinnedTasks = Template.bind({});
 WithPinnedTasks.args = {
     tasks: [
         ...Default.args.tasks.slice(0, 5),
-        { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+        {id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED"},
     ],
 };
 
